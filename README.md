98904d255b1be77f6585699488c2ffe8098f5502
Eirik Dahlen <dahleneirik@gmail.com>
2020-12-08 11:22:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exploring attention-based models for classification of pro-eating disorder users on Twitter
+
+In this project we fine-tune the pre-trained attention-based models BERT, ALBERT and DisilBERT and compare it to a baseline SVM classifier on the multiclass task of classifying pro-eating disorder users on Twitter.
+
+To set up project using the correct Python packages, run this command
+
+```
+pip install -r requirements.txt
+```
---
+++ distilbert.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+import numpy as np
+import time
+import tensorflow as tf
+from transformers import DistilBertTokenizerFast, TFDistilBertForSequenceClassification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+start = time.time()
+
+df = pd.read_csv("frikk_eirik_dataset.csv")
+
+X, y = df['text_document'], df["target"]
+
+X_a, X_b, y_a, y_b = train_test_split(X, y, train_size=0.8, random_state=42, stratify=df['target'])
+
+X_list = X_a.values.tolist()
+for i in range(len(X_list)):
+    X_list[i] = str(X_list[i])
+X_val = X_b.values.tolist()
+for i in range(len(X_val)):
+    X_val[i] = str(X_val[i])
+
+
+y_list = y_a.values.tolist()
+labels_dict = {'unrelated': 0, 'pro_ed': 1, 'pro_recovery': 2}
+for i in range(len(y_list)):
+    y_list[i] = labels_dict[y_list[i]]
+
+
+y_val = y_b.values.tolist()
+for i in range(len(y_val)):
+    y_val[i] = labels_dict[y_val[i]]
+
+time_a = time.time() - start
+
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+train_encodings = tokenizer(X_list, truncation=True, padding=True)
+time_b = time.time() - time_a - start
+print(f"Created train encodings, time used {time_b}")
+val_encodings = tokenizer(X_val, truncation=True, padding=True)
+time_c = time.time() - time_b - time_a - start
+print(f"Created val encodings, time used {time_c}")
+
+train_dataset = np.array(list(dict(train_encodings).values()))
+val_dataset = np.array(list(dict(val_encodings).values()))
+
+BATCH_SIZE = 16
+
+# Create a callback that saves the model's weights every x epochs
+checkpoint_path = "distilbert16_ckpt/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True)
+
+save_model = True
+
+model = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=3, return_dict=True)
+
+if save_model:
+    optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
+    model.compile(optimizer=optimizer, loss=model.compute_loss, metrics=['accuracy'])
+
+    model.fit(
+        train_dataset[0],
+        np.array(y_list),
+        epochs=5,
+        batch_size=BATCH_SIZE,
+        callbacks=[cp_callback]
+        )
+else:
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
+
+preds = model.predict(val_dataset[0])["logits"]
+
+preds_proba = tf.keras.backend.softmax(preds, axis=1)
+
+classes = np.argmax(preds, axis=-1)
+
+score = classification_report(y_val, classes, digits=3)
+print(score)
+
+total = time.time()  - start
+print(f"Done in: {total}")  
